023d5d558f5c2264bf064b3900772010193e81fa
Wouter <vanunen.wouter@gmail.com>
2016-04-06 14:27:06 +0000

    Pasific.

---
+++ _OLD/js/script.js
@@ -0,0 +1,20 @@
+var tbapp = angular.module('tb-app', ['mgo-angular-wizard']);
+
+tbapp.controller('TbCtrl', function ($scope) {
+    $scope.yes = 'true';
+    $scope.no = 'true';
+
+    $scope.getValue=function(item){
+        var value = angular.element(item).data('value');
+        console.log(value);
+    };
+
+    $scope.enableStep=function (name) {
+        if(name == 'yes'){
+            $scope.yes = 'false';
+        }
+        if(name == 'no'){
+            $scope.no ='false';
+        }
+    }
+});
